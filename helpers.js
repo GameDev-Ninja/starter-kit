59d264a10c7e1c2fa249d5dd8eaf2b46befc89e4
Romain 'Maz' BILLOIR <contact@maz.digital>
2022-07-19 01:40:21 +0200

    Ajout de drawPolygon, drawCircle et drawLine

--- helpers.js
+++ helpers.js
@@ -150,7 +150,7 @@ circle = (center, radius) => ({center, radius})
  * @param including
  * @returns {boolean}
  */
-Number.prototype.isBetween = (a, b, including) => isNumberBetween(a, this, b, including)
+Number.prototype.isBetween = function (a, b, including) { return isNumberBetween(a, this, b, including) }
 
 /**
  * Circonscrit le nombre entre un minimum et un maximum
@@ -158,7 +158,7 @@ Number.prototype.isBetween = (a, b, including) => isNumberBetween(a, this, b, in
  * @param max
  * @returns {number}
  */
-Number.prototype.clamp = (min, max) => clamp(min, this, max)
+Number.prototype.clamp = function (min, max) { return clamp(min, this, max) }
 
 
 /**
@@ -171,3 +171,24 @@ Number.prototype.clamp = (min, max) => clamp(min, this, max)
  * @returns {boolean}
  */
 Boolean.random = _ => Math.random() < .5
+
+/**
+ * Extension du CanvasRenderingContext2D
+ */
+CanvasRenderingContext2D.prototype.drawPolygon = function (points) {
+    this.beginPath()
+    this.moveTo(points[0].x, points[0].y)
+    points.slice(1).forEach(point => this.lineTo(point.x, point.y))
+    this.closePath()
+}
+
+CanvasRenderingContext2D.prototype.drawCircle = function (center, radius, startAngle = 0, angle = 2*Math.PI, antiClockWise = true) {
+    this.beginPath()
+    this.arc(center.x, center.y, radius, startAngle, angle, antiClockWise)
+}
+
+CanvasRenderingContext2D.prototype.drawLine = function (from, to) {
+    this.beginPath();
+    this.moveTo(from.x, from.y);
+    this.lineTo(to.x, to.y);
+}
