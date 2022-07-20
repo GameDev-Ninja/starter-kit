4a04f8befda079bf9c73b7262efab5e5067fa505
Romain 'Maz' BILLOIR <contact@maz.digital>
2022-07-20 22:19:13 +0200

    Return events removers in keyDown, keyUp and keyDownUp

--- input-system.js
+++ input-system.js
@@ -1,17 +1,15 @@
 /**
  * Ces trois fonctions facilitent la gestion du clavier
  */
-// Stock les `keyCode` des touches actuellement enfoncées
+// Stocke les `keyCode` des touches actuellement enfoncées
 let downKeys = []
 
 // Lors d'une pression, ajoute le `keyCode` à la liste
 document.addEventListener("keydown", e => {
     e.preventDefault()
 
-    let downKeysIndex = downKeys.findIndex(keyCode => keyCode === e.code)
-    if (downKeysIndex !== -1) return
-
-    downKeys.push(e.code)
+    if (!e.repeat)
+        downKeys.push(e.code)
 })
 
 // Lors du relâchement de pression, retire le keyCode de la liste
@@ -25,23 +23,58 @@ document.addEventListener("keyup", e => {
 // Vide la liste lorsque la page n'est plus affichée
 window.addEventListener("blur", e => downKeys = [])
 
-// Indique si une touche est enfoncée ou non
-const isKeyDown = searchedCode => downKeys.findIndex(keyCode => keyCode === searchedCode) !== -1
+/**
+ * Indique si une touche est enfoncée ou non
+ * @param searchedKeyCode
+ * @returns {boolean}
+ */
+const isKeyDown = searchedKeyCode => downKeys.findIndex(keyCode => keyCode === searchedKeyCode) !== -1
 
-// Assigne une fonction à la pression d'une touche
-const keyDown = (keyCode, callback) =>
-    document.addEventListener("keydown", e => {
+/**
+ * Assigne une fonction à l'enfoncement d'une touche,
+ * Retourne une fonction de désactivation de l'événement
+ * @param keyCode
+ * @param callback
+ * @returns {function(): void}
+ */
+const keyDown = (keyCode, callback) => {
+    let cb = e => {
+        e.preventDefault()
+
         if (e.code === keyCode && !e.repeat) callback(e)
-    })
+    }
+    document.addEventListener("keydown", cb)
 
-// Assigne une fonction au relâchement d'une touche
-const keyUp = (keyCode, callback) =>
-    document.addEventListener("keyup", e => {
-        if (e.code === keyCode) callback(e)
-    })
-
-// Raccourci pour assigner deux fonctions à la pression/relâchement d'une touche
-const keyUpDown = (keyCode, callbackDown, callbackUp) => {
-    keyDown(keyCode, callbackDown)
-    keyUp(keyCode, callbackUp)
+    return () => document.removeEventListener("keydown", cb)
 }
+
+/**
+ * Assigne une fonction au relâchement d'une touche,
+ * Retourne une fonction de désactivation de l'événement
+ * @param keyCode
+ * @param callback
+ * @returns {function(): void}
+ */
+const keyUp = (keyCode, callback) => {
+    let cb = e => {
+        e.preventDefault()
+
+        if (e.code === keyCode) callback(e)
+    }
+    document.addEventListener("keyup", cb)
+
+    return () => document.removeEventListener("keydown", cb)
+}
+
+/**
+ * Raccourci pour assigner deux fonctions à l'enfoncement et relâchement d'une touche
+ * Retourne un objet { down, up } contenant les fonctions de désactivation des événements
+ * @param keyCode
+ * @param callbackDown
+ * @param callbackUp
+ * @returns {{up: (function(): void), down: (function(): void)}}
+ */
+const keyDownUp = (keyCode, callbackDown, callbackUp) => ({
+    down: keyDown(keyCode, callbackDown),
+    up: keyUp(keyCode, callbackUp)
+})
